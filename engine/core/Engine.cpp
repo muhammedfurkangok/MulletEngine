fafc51a55757df3769af1e88d500ed8b5264af49
Muhammed Furkan Gok <gkkangk@gmail.com>
2025-08-29 22:53:14 +0300

    ref: CMake structure and add Engine core files

---
+++ engine/core/Engine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gkkan on 29.08.2025.
+//
+
+#include "Engine.hpp"
---
+++ engine/core/Engine.hpp
@@ -0,0 +1,15 @@
+//
+// Created by gkkan on 29.08.2025.
+//
+
+#ifndef MULLETENGINE_ENGINE_HPP
+#define MULLETENGINE_ENGINE_HPP
+
+
+class Engine
+{
+
+};
+
+
+#endif //MULLETENGINE_ENGINE_HPP
