7e419389f65626ab94f5321bcfe2cb8232df2190
Muhammed Furkan Gok <gkkangk@gmail.com>
2025-08-31 18:33:02 +0300

    ref: main.cpp with improved OpenGL setup and rendering

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -1,80 +1,141 @@
-    #include <glad/glad.h>
-    #include <GLFW/glfw3.h>
-    #include <iostream>
-    #include "file_manager/FileManager.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "file_manager/FileManager.h"
+#include "shader/shader.h"
 
 
+#include <iostream>
+#include <ctime>
 
-int main()
+
+static void glfw_error_callback(int error, const char *description)
+{
+    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+static void print_start_info()
+{
+    // current date/time based on current system
+    time_t now = time(0);
+    char *date_time = ctime(&now);
+    // version, format -> x.xx.xxx
+    std::string version("000001");
+
+    std::cout << "enigine_version: " << version << std::endl;
+    std::cout << "cpp_version: " << __cplusplus << std::endl;
+    std::cout << "started_at: " << date_time << std::endl;
+}
+
+void create_triangle(unsigned int &vbo, unsigned int &vao, unsigned int &ebo)
+{
+    // create the triangle
+    float triangle_vertices[] = {
+            0.0f, 0.25f, 0.0f,    // position vertex 1
+            1.0f, 0.0f, 0.0f,     // color vertex 1
+            0.25f, -0.25f, 0.0f,  // position vertex 1
+            0.0f, 1.0f, 0.0f,     // color vertex 1
+            -0.25f, -0.25f, 0.0f, // position vertex 1
+            0.0f, 0.0f, 1.0f,     // color vertex 1
+    };
+    unsigned int triangle_indices[] = {
+            0, 1, 2};
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertices), triangle_vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(triangle_indices), triangle_indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) 0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+int main(int argc, char **argv)
+{
+    print_start_info();
+
+    // Setup window
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+
+// Decide GL+GLSL versions
+#if __APPLE__
+    // GL 3.2 + GLSL 150
+const char *glsl_version = "#version 150";
+glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
+glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           // Required on Mac
+#else
+    // GL 3.0 + GLSL 130
+    const char *glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+    // Create window with graphics context
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "enigine", NULL, NULL);
+    if (window == NULL)
+        return 1;
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    bool err = gladLoadGL() == 0;
+    if (err)
     {
-        glfwInit();
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-
-        constexpr int window_width = 800;
-        constexpr int window_height = 600;
-        FileManager* fileManager;
-        GLFWwindow *window = glfwCreateWindow(window_width, window_height, "Triangle", nullptr, nullptr);
-        glfwMakeContextCurrent(window);
-        gladLoadGL();
-        glViewport(0, 0, window_width, window_height);
-
-        std::string vertex_stage_text = FileManager::readFile("assets/shaders/temp-shader.vs");
-        std::string fragment_stage_text = FileManager::readFile("assets/shaders/temp-shader.fs");
-
-        GLuint vertex_stage = glCreateShader(GL_VERTEX_SHADER);
-        const char* vertex_src = vertex_stage_text.c_str();
-        glShaderSource(vertex_stage, 1, &vertex_src, nullptr);
-        glCompileShader(vertex_stage);
-
-        GLuint fragment_stage = glCreateShader(GL_FRAGMENT_SHADER);
-        const char* fragment_src = fragment_stage_text.c_str();
-        glShaderSource(fragment_stage, 1, &fragment_src, nullptr);
-        glCompileShader(fragment_stage);
-
-        GLuint shader = glCreateProgram();
-        glAttachShader(shader, vertex_stage);
-        glAttachShader(shader, fragment_stage);
-        glLinkProgram(shader);
-
-        float vertices[] = {
-                -0.5f, -0.5f, 0.0f,
-                0.5f, -0.5f, 0.0f,
-                0.0f, 0.5f, 0.0f
-        };
-
-        GLuint vao, vbo;
-        glGenVertexArrays(1, &vao);
-        glBindVertexArray(vao);
-
-        glGenBuffers(1, &vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
-        glEnableVertexAttribArray(0);
-
-        while (!glfwWindowShouldClose(window))
-        {
-            glfwPollEvents();
-
-            glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
-
-            glUseProgram(shader);
-            glBindVertexArray(vao);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
-
-            glfwSwapBuffers(window);
-        }
-
-        glDeleteVertexArrays(1, &vao);
-        glDeleteBuffers(1, &vbo);
-        glDeleteShader(vertex_stage);
-        glDeleteShader(fragment_stage);
-        glDeleteProgram(shader);
-
-        glfwDestroyWindow(window);
-        glfwTerminate();
-
-        return 0;
+        fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+        return 1;
     }
+
+    if (err)
+    {
+        fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+        return 1;
+    }
+
+    int screen_width, screen_height;
+    glfwGetFramebufferSize(window, &screen_width, &screen_height);
+    glViewport(0, 0, screen_width, screen_height);
+
+    // Create geometries
+    unsigned int vbo, vao, ebo;
+    create_triangle(vbo, vao, ebo);
+
+    // Init shader
+    Shader triangle_shader;
+    triangle_shader.init(FileManager::readFile("assets/shaders/simple-shader.vs"),
+                         FileManager::readFile(("assets/shaders/simple-shader.fs")));
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // render geometries
+        triangle_shader.use();
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
