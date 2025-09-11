dab9020a49550dc732237c1a7ce1474d7bdf1038
Muhammed Furkan Gok <gkkangk@gmail.com>
2025-09-11 23:32:12 +0300

    feat: add private members and draw method to Model class

--- engine/model/model.h
+++ engine/model/model.h
@@ -26,8 +26,21 @@ public:
 
     Model(const Model&) = delete;
     Model& operator=(const Model&) = delete;
-
     Model(Model&&) = delete;
     Model& operator=(Model&&) = delete;
 
+    void draw(const Shader& shader) const;
+
+    
+
+private:
+    std::vector<Texture> textures_loaded_;
+    std::vector<Mesh> meshes_;
+    std::string directory_;
+    bool gammaCorrection_ = false;
+
+    void loadModel(const std::string& path);
+    void processNode(aiNode* node, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, const std::string& typeName);
 };
